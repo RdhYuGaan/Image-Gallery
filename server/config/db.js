11854921bad607f74c4380f711fe8253b654c586
RdhYuGaan <radithyugan22@gmail.com>
2025-06-12 14:30:08 +0530

    connect databse and fix errors

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'your_password',
+  password: '',
   database: 'image_gallery'
 });
 
